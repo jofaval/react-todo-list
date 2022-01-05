d60084f91efe21a0f0f29e665eeffce881ba0e80
pepe_fabra_valverde <jofaval@outlook.com>
2022-01-05 17:02:37 +0100

    Create basic unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReduxProvider from './store/Provider';
 
-test('renders learn react link', () => {
+test('renders to-do list app', () => {
   expect(true).toBe(true);
+  render(<React.StrictMode>
+    <ReduxProvider>
+      <App />
+    </ReduxProvider>
+  </React.StrictMode>);
+
+  const titleElement = screen.getByText(/To\-Do List/i);
+  expect(titleElement).toBeInTheDocument();
 });
--- src/Task.test.tsx
+++ src/Task.test.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { removeTask } from "src/store/actionCreator";
+import Task from './components/Task/Task';
+import ReduxProvider from './store/Provider';
+
+const ToTest: React.FC = () => {
+  return <ReduxProvider>
+    <Task task={{
+      title: 'Test',
+      complete: false
+    }} />
+  </ReduxProvider>
+}
 
 test('renders a task', () => {
   expect(true).toBe(true);
+  render(<ToTest />);
+
+  const taskElement = screen.getByText(/Test/)
+  expect(taskElement).toBeInTheDocument();
 });
